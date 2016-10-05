0c0847569180ee4c7b264a149be36cf8cb784903
Heather Conley <hconley6695@gmail.com>
2016-10-05 13:33:04 -0400

    got clear button to empty screen

--- main.js
+++ main.js
@@ -2,7 +2,44 @@
 var buttonList = document.querySelector('.buttons');
 var showme = document.querySelector('#shown');
 
+function perfCalc(num1, num2) {
+		if (num === "+") {
+			return num1 + num2;
+		} else if (num === "-") {
+			return num1 - num2; 
+		} else if (num === "*") {
+			return num1 * num2;
+		} else if (num === "/") {
+			return num1 / num2;
+		} else {
+			return "I don't know what you are doing!"
+		}
+}
+
+function clearout() {
+	showme.textContent = " ";
+}
+
 buttonList.addEventListener('click', function(event) {
-	var number = event.target.textContent;
-	showme.textContent = number;
-})
+	var num = event.target.textContent;
+	showme.textContent = num;
+
+	// if (num >= 0) {
+		
+	// }
+
+	// var num1 = Number(num);
+	// console.log(num1);
+
+	// var num2 = Number(num);
+	// console.log(num2);
+	
+	if (num === "C") {
+		clearout();
+	}
+
+
+
+});
+
+
